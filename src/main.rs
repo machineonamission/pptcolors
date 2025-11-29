2b5b2f8318ce7bd083d9c6359b29ae940f9304d9
Melody <machineonamission@gmail.com>
2025-11-29 17:46:08 -0600

    gjhfdhj

--- src/main.rs
+++ src/main.rs
@@ -188,13 +188,15 @@ fn main() {
     //     }
     // }
     let critical_values: Vec<u8> = [
-        1, 2, 4, 7, 8, 11, 13, 14, 16, 19, 22, 23, 26, 28, 29, 31, 32, 37, 38, 41, 43, 44, 46, 47,
-        49, 52, 53, 56, 58, 59, 61, 62, 64, 67, 71, 73, 74, 76, 77, 79, 82, 83, 86, 88, 89, 91, 92,
-        94, 97, 98, 101, 103, 104, 106, 107, 109, 112, 113, 116, 118, 121, 122, 124, 127, 128, 131,
-        133, 134, 137, 139, 142, 143, 146, 148, 149, 151, 152, 154, 157, 158, 161, 163, 164, 166,
-        167, 169, 172, 173, 176, 178, 179, 181, 182, 184, 188, 191, 193, 194, 196, 197, 199, 202,
-        203, 206, 208, 209, 211, 212, 214, 217, 218, 223, 224, 226, 227, 229, 232, 233, 236, 239,
-        241, 242, 244, 247, 248, 251, 253, 254,
+        1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 35, 37, 38,
+        40, 41, 43, 44, 46, 47, 49, 50, 52, 53, 55, 56, 58, 59, 61, 62, 64, 65, 67, 70, 71, 73, 74,
+        76, 77, 79, 80, 82, 83, 86, 88, 89, 91, 92, 94, 95, 97, 98, 100, 101, 103, 104, 106, 107,
+        109, 110, 112, 113, 115, 116, 118, 121, 122, 124, 125, 127, 128, 130, 131, 133, 134, 137,
+        139, 140, 142, 143, 145, 146, 148, 149, 151, 152, 154, 155, 157, 158, 160, 161, 163, 164,
+        166, 167, 169, 172, 173, 175, 176, 178, 179, 181, 182, 184, 185, 188, 190, 191, 193, 194,
+        196, 197, 199, 200, 202, 203, 205, 206, 208, 209, 211, 212, 214, 215, 217, 218, 220, 223,
+        224, 226, 227, 229, 230, 232, 233, 235, 236, 239, 241, 242, 244, 245, 247, 248, 250, 251,
+        253, 254,
     ]
     .try_into()
     .unwrap();
@@ -235,7 +237,6 @@ fn main() {
     let constructions: Vec<OnceLock<ColorMix>> = std::iter::repeat_with(OnceLock::new)
         .take(TOTAL_COLORS)
         .collect();
-
 
     for color in &colors {
         constructions[color.to_index() as usize].set(ColorMix::Base(color.to_index()));
@@ -282,7 +283,6 @@ fn main() {
                                 .set(ColorMix::Mixed(construction.clone()))
                                 .is_ok()
                             {
-
                                 let t = total.fetch_add(1, Ordering::Relaxed);
                                 // if t & (2<<20)-1 == 0 {
                                 //     std::process::exit(0);
@@ -297,13 +297,15 @@ fn main() {
                                     for c_r in critical_values.clone() {
                                         for c_g in critical_values.clone() {
                                             'outer: for c_b in critical_values.clone() {
-                                                for r in [0,c_r,255] {
-                                                    for g in [0,c_g,255] {
-                                                        for b in [0,c_b,255] {
-                                                            let col = ColorInt {
-                                                                r,g,b
-                                                            };
-                                                            if constructions[col.to_index() as usize].get().is_none() {
+                                                for r in [0, c_r, 255] {
+                                                    for g in [0, c_g, 255] {
+                                                        for b in [0, c_b, 255] {
+                                                            let col = ColorInt { r, g, b };
+                                                            if constructions
+                                                                [col.to_index() as usize]
+                                                                .get()
+                                                                .is_none()
+                                                            {
                                                                 continue 'outer;
                                                             }
                                                         }
@@ -314,7 +316,6 @@ fn main() {
                                             }
                                         }
                                     }
-
                                 }
                                 if t == TOTAL_COLORS as u32 {
                                     println!("All colors constructed!");
