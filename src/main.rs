969b8c97ee82b908ae3a47869f0f6eefcc24cbe1
Melody <machineonamission@gmail.com>
2025-11-29 00:41:18 -0600

    fghajh

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ const INPUT: [&str; 143] = [
     "FFCC99", "FFCCCC", "FFCCFF", "FFFF00", "FFFF66", "FFFF99", "FFFFCC", "FFFFFF",
 ];
 
-#[derive(Eq, Hash, PartialEq, Clone)]
+#[derive(Eq, Hash, PartialEq, Clone, Debug)]
 struct ColorInt {
     r: u8,
     g: u8,
@@ -162,17 +162,33 @@ fn main() {
     // let combination_mixed: Vec<Vec<AtomicBool>> = (0..TOTAL_COLORS)
     //     .map(|_| (0..TOTAL_COLORS).map(|_| AtomicBool::new(false)).collect())
     //     .collect();
-
+    let interesting_values = [0u8,1u8,255u8];
+    let mut interesting_colors: Vec<ColorInt> = vec!();
+    for r in &interesting_values {
+        for g in &interesting_values {
+            for b in &interesting_values {
+                let color = ColorInt { r: *r, g: *g, b: *b };
+                interesting_colors.push(color);
+            }
+        }
+    }
+    let interesting_indices: Vec<u32> = interesting_colors.iter().map(|c| c.to_index()).collect();
     let constructions: Vec<OnceLock<ColorMix>> = std::iter::repeat_with(OnceLock::new)
         .take(TOTAL_COLORS)
         .collect();
 
     // let mut int_constructions: FxHashSet<ColorFractions> = FxHashSet::default();
 
+    let mut interesting_total = AtomicU32::new(0);
+
     for color in &colors {
+        if interesting_indices.contains(&(color.to_index())) {
+            interesting_total.fetch_add(1, Ordering::Relaxed);
+        }
         constructions[color.to_index() as usize].set(ColorMix::Base(color.to_index()));
         // int_constructions.insert(color.clone());
     }
+    let mut total = AtomicU32::new(0);
     let mut total = AtomicU32::new(0);
     let mut iteration = 1;
     let mut next_constructions = constructions.clone();
@@ -208,18 +224,24 @@ fn main() {
                                 transparency: transparency.clone(),
                                 steps: 0, //max(const1_steps, const2_steps) + 1,
                             };
-                            constructions[mixed_index].set(ColorMix::Mixed(construction.clone()));
-                            next_constructions[mixed_index].set(ColorMix::Mixed(construction));
-                            let t = total.fetch_add(1, Ordering::Relaxed);
-                            // if t & (2<<20)-1 == 0 {
-                            //     std::process::exit(0);
-                            // }
-                            if t & (2<<16)-1 == 0 {
-                                println!("Constructions found: {} ({}%)", t, t as f64/(TOTAL_COLORS as f64) * 100.0);
-                            }
-                            if t == TOTAL_COLORS as u32 {
-                                println!("All colors constructed!");
-                                std::process::exit(0);
+                            if constructions[mixed_index].set(ColorMix::Mixed(construction.clone())).is_ok() {
+                                next_constructions[mixed_index].set(ColorMix::Mixed(construction));
+                                if interesting_indices.contains(&(mixed_index as u32)) {
+                                    let it = interesting_total.fetch_add(1, Ordering::Relaxed);
+                                    println!("Interesting constructions found: {}", it);
+                                    println!("{:?} = {:?} + {:?} ({}%)", mixed_color, ColorInt::from_index(*color), other_color, transparency)
+                                }
+                                let t = total.fetch_add(1, Ordering::Relaxed);
+                                // if t & (2<<20)-1 == 0 {
+                                //     std::process::exit(0);
+                                // }
+                                if t & (2<<16)-1 == 0 {
+                                    println!("Constructions found: {} ({}%)", t, t as f64/(TOTAL_COLORS as f64) * 100.0);
+                                }
+                                if t == TOTAL_COLORS as u32 {
+                                    println!("All colors constructed!");
+                                    std::process::exit(0);
+                                }
                             }
                         }
                     }
